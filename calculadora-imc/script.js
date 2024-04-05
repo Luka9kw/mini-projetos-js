baf946c1d68adb2549b90e37b82c8cf529cb4be4
Luka9kw <luka9kw@hotmail.com>
2024-04-05 10:25:12 -0300

    calculadora IMC finalizado

--- calculadora-imc/script.js
+++ calculadora-imc/script.js
@@ -1,21 +1,37 @@
-
-
 let btn = document.querySelector('#btn');
-btn.addEventListener('click', calculateImc);
-let imcResult = '';
-function calculateImc() {
+
+
+function calcularImc() {
     let nome = document.querySelector('input#nome').value;
     let altura = document.querySelector('input#altura').value;
-    let peso = document.querySelector('input#peso').value;
+    let peso = document.querySelector('input#peso').value; 
+    let resultado = document.querySelector('.result');
+    resultado.textContent = '';
 
-    imcResult = peso / (altura * altura);
-    console.log(imcResult);
+    if (nome == '' || altura == '' || peso == '') {
+        return resultado.textContent = 'Por favor, preencha todos os espaços.';
+    }
 
-    let result = document.querySelector('.result');
+    // Resultados
+    const imc = (peso / (altura * altura)).toFixed(1);
 
-    let resultText = document.createElement("p");
-    result.appendChild(resultText);
-
-    resultText.innerHTML = `Seu IMC é de ${imcResult}`;
+    if (imc < 18.5) {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está abaixo do peso. Cuidado!`
+    } else if (imc >= 18, 5 && imc <= 24, 9) {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com o peso ideal. Parabéns!`
+    } else if (imc >= 25 && imc <= 29, 9) {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com sobrepeso. Cuidado!`
+    } else if (imc >= 30 && imc <= 34, 9) {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau I. Muito Cuidado!`
+    } else if (imc >= 35 && 39, 9) {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau II. Muito Cuidado!`
+    } else {
+        resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau III. Muito Cuidado!`
+    };
 };
+
+btn.addEventListener('click', calcularImc);
+
+
+
 
