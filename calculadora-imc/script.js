1da450e2e5ea61996e97da66dc11c8655743a604
luka9kw <luka9kw@hotmail.com>
2024-04-05 16:52:31 -0300

    resultados corrigidos, valores estavam utilizando , ao inves de .

--- calculadora-imc/script.js
+++ calculadora-imc/script.js
@@ -17,13 +17,13 @@ function calcularImc() {
 
     if (imc < 18.5) {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está abaixo do peso. Cuidado!`
-    } else if (imc >= 18, 5 && imc <= 24, 9) {
+    } else if (imc >= 18.5 && imc <= 24.9) {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com o peso ideal. Parabéns!`
-    } else if (imc >= 25 && imc <= 29, 9) {
+    } else if (imc >= 25 && imc <= 29.9) {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com sobrepeso. Cuidado!`
-    } else if (imc >= 30 && imc <= 34, 9) {
+    } else if (imc >= 30 && imc <= 34.9) {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau I. Muito Cuidado!`
-    } else if (imc >= 35 && 39, 9) {
+    } else if (imc >= 35 && 39.9) {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau II. Muito Cuidado!`
     } else {
         resultado.textContent = `${nome}, seu IMC é de ${imc}. Você está com obesidade grau III. Muito Cuidado!`
